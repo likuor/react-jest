9e58753ea9ad2b1a91bbe4b25399ec74aafdfed7
likuor <likuor0528@gmail.com>
2024-05-12 17:38:38 -0700

    add: form test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Button from './components/Button';
+// import Button from './components/Button';
+import Form from './components/Form';
 
 function App() {
   return (
     <>
-      <Button label='button' onClick={() => alert('click')} />
+      {/* <Button label='button' onClick={() => alert('click')} /> */}
+      <Form />
     </>
   );
 }
