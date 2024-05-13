8ee5e12c049ea1e5af457211114f91c7b1c2c830
likuor <likuor0528@gmail.com>
2024-05-12 17:46:33 -0700

    add: async

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import Button from './components/Button';
-import Form from './components/Form';
+import AsyncComponent from './components/AsyncComponent';
+// import Form from './components/Form';
 
 function App() {
   return (
     <>
       {/* <Button label='button' onClick={() => alert('click')} /> */}
-      <Form />
+      {/* <Form /> */}
+      <AsyncComponent />
     </>
   );
 }
